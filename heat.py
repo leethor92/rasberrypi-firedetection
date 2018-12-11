581471700c7f32494f11bae27e772cff21e4f9d9
leethor92 <leethornton1992@hotmail.com>
2018-12-11 16:57:41 +0000

    added WIA functionality

--- heat.py
+++ heat.py
@@ -1,18 +1,21 @@
 from sense_hat import SenseHat
+from wia import Wia
 import time
 
+wia = Wia()
+wia.access_token = "d_sk_A0AZjrtuvsYqWmp1WbVXStVS"
 sense = SenseHat()
 sense.clear()
 green = (0,255,0)
 orange = (255,165,0)
 red = (255,0,0)
 while True:
-  temp = sense.get_temperature()
+  temp=round(sense.get_temperature(),2)
   if temp >=50:
     sense.show_message("FIRE!",text_colour = red)
   elif temp >=35:
     sense.show_message("warning",text_colour = orange)
   else:
     sense.show_message("Fine!",text_colour = green)
-  print(temp)
   time.sleep(10)
+  wia.Event.publish(name="temperature",data=temp)
