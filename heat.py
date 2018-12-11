548e178afcb2bff66a261afaa603c195ca79c570
leethor92 <leethornton1992@hotmail.com>
2018-12-11 15:59:43 +0000

    add heat.py

---
+++ heat.py
@@ -0,0 +1,17 @@
+from sense_hat import SenseHat
+
+sense = SenseHat()
+sense.clear()
+green = (0,255,0)
+orange = (255,165,0)
+red = (255,0,0)
+while True:
+  temp = sense.get_temperature()
+  if temp >=50:
+    sense.show_message("FIRE!",text_colour = red)
+  elif temp >=35:
+    sense.show_message("warning",text_colour = orange)
+  else:
+    sense.show_message("Fine!",text_colour = green)
+  print(temp)
+ 
